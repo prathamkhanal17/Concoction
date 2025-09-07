0c5f3d93027c1f14131e66d120c286729b063e86
Pratham Khanal <prathamkhanal962@gmail.com>
2025-09-07 15:40:48 +0545

    fixing path

--- hypr/.config/hypr/scripts/set_wallpaper.sh
+++ hypr/.config/hypr/scripts/set_wallpaper.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 # Path to your wallpaper image
-WALLPAPER="/home/zoro/Pictures/wallpapers/gr.png"
+WALLPAPER="/home/pratham/Pictures/wallpapers/skull-3.png"
 
 # Check if swww is running, start it if not
-if ! pgrep -x "swww-daemon" > /dev/null; then
-    echo "Starting swww daemon..."
-    swww-daemon &
-    sleep 1  # Give it a second to initialize
+if ! pgrep -x "swww-daemon" >/dev/null; then
+  echo "Starting swww daemon..."
+  swww-daemon &
+  sleep 1 # Give it a second to initialize
 fi
 
 # Set the wallpaper with a fade transition
 swww img "$WALLPAPER" --transition-type any --transition-fps 60 --transition-duration 1
 
 echo "Wallpaper set to: $WALLPAPER"
-
